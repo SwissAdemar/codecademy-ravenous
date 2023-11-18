fd5660b0d8758b9afd4548f3aa486077562b3103
SwissAdemar <66174125+SwissAdemar@users.noreply.github.com>
2023-11-18 23:26:59 +0100

    add states functionality so that form data can be stored

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -7,10 +7,21 @@ const SearchBar = () => {
     const [selected, setSelected] = useState(null);
 
     const handleSelect = (e) => {
-        setSelected(e.target.value);
+        setSelected(e.target.id);
     }
 
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+    }
     
+    const handleLocation = (e) => {
+        setLocation(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`Searching Yelp with ${search}, ${location}, ${selected}`);
+    }
 
     
     return (
@@ -19,23 +30,23 @@ const SearchBar = () => {
                 <button 
                     className={`sort-btn ${selected === 'best_match' && 'active'}`}
                     onClick={handleSelect} 
-                    value="best_match">Best Match
+                    id="best_match">Best Match
                 </button>
                 <button 
                     className={`sort-btn ${selected === 'rating' && 'active'}`} 
                     onClick={handleSelect} 
-                    value="rating">Highest Rated
+                    id="rating">Highest Rated
                 </button>
                 <button 
-                    className={`sort-btn ${selected === 'review-count' && 'active'}`} 
+                    className={`sort-btn ${selected === 'review_count' && 'active'}`} 
                     onClick={handleSelect} 
-                    value="review-count">Most Reviewed
+                    id="review_count">Most Reviewed
                 </button>
             </div>
-            <form className="form-content">
+            <form className="form-content" onSubmit={handleSubmit}>
                 <div className="form-inputs">
-                    <input type="text" placeholder="Search Business" className="form-input" />
-                    <input type="text" placeholder="Where?" className="form-input" />
+                    <input type="text" placeholder="Search Business" className="form-input" value={search} onChange={handleSearch}/>
+                    <input type="text" placeholder="Where?" className="form-input" value={location} onChange={handleLocation}/>
                 </div>
                 <input type="submit" className="submit-btn" value="Let's Go"/>
             </form>
